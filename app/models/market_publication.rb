d746536a25c8dc85f58fd6faba856e3302aa352b
Ryan Verhey <rverhey@gannett.com>
2016-08-17 15:49:55 -0400

    SCOPS-965: Added MarketPublication::all for returning sorted vindicia select sites

--- app/models/market_publication.rb
+++ app/models/market_publication.rb
@@ -1,3 +1,7 @@
 class MarketPublication
-
+  def self.all(sort_by: :pub_code)
+    VINDICIA_SELECT_SITES.sort_by do |site|
+      site[sort_by] || site[:pub_code]
+    end
+  end
 end
--- test/models/market_publication_test.rb
+++ test/models/market_publication_test.rb
@@ -1,5 +1,23 @@
 require 'test_helper'
 
 class MarketPublicationTest < ActiveSupport::TestCase
+  def setup
+    @sites = VINDICIA_SELECT_SITES
+    @pub_sorted_sites = @sites.sort_by { |site| site[:pub_code] }
+    @gci_sorted_sites = @sites.sort_by { |site| site[:gci_unit] }
+  end
 
+  class TestAllMethod < MarketPublicationTest
+    test "::all method returns the VINDICIA_SELECT_SITES sorted by pub code" do
+      assert_equal MarketPublication.all, @pub_sorted_sites
+    end
+
+    test "::all method can also sort by gci unit" do
+      assert_equal MarketPublication.all(sort_by: :gci_unit), @gci_sorted_sites
+    end
+
+    test "::all sorts by pub code if invalid sorting parameter is passed" do
+      assert_equal MarketPublication.all(sort_by: :not_valid), @pub_sorted_sites
+    end
+  end
 end

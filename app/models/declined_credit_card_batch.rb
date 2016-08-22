f979ea8e77d868c341151459eb0488f67ea9abab
James Moore <jrmoore@gannett.com>
2016-08-22 12:33:29 -0400

    SLSMKT-966

--- app/models/declined_credit_card_batch.rb
+++ app/models/declined_credit_card_batch.rb
@@ -1,6 +1,8 @@
 class DeclinedCreditCardBatch < ActiveRecord::Base
   before_create :set_defaults
+
   has_many :declined_credit_card_transactions
+
   def set_defaults
     self.status = "New"
     self.run_timestamp = Time.now
--- app/services/generate_file.rb
+++ app/services/generate_file.rb
@@ -1,24 +1,22 @@
 class GenerateFile
 
-  attr_accessor :gci_unit, :pub_code, :run_date
+  attr_accessor :run_date
 
-  def initialize(gci_unit:, pub_code:)
-    @gci_unit = gci_unit
-    @pub_code = pub_code
+  def initialize
     @run_date = Date.today
   end
 
   def include_markets_and_pub
-    MarketPublciation.all
+    MarketPublication.all
   end
 
   def process
     batch = DeclinedCreditCardBatch.new
-    transactions = batch.build
-    # include_markets_and_pub.each do |gci_and_pub|
-    #   transactions = DeclinedCreditCard.summary(gci_and_pub[:gci_unit]).map do |declined_cc|
-    #     DeclinedCreditCardTransaction.new
-    #   end
-    # end
+    transactions = []
+    include_markets_and_pub.each do |gci_and_pub|
+      DeclinedCreditCard.summary(gci_and_pub[:gci_unit].to_s).map do |declined_cc|
+        puts batch.declined_credit_card_transactions.build(declined_cc)
+      end
+    end
   end
 end

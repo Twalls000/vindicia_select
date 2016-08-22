038012bab6c26a223db2abda279bcd87fc48fc17
James Moore <jrmoore@gannett.com>
2016-08-21 21:41:02 -0400

    SLSMKT-966

--- app/models/declined_credit_card.rb
+++ app/models/declined_credit_card.rb
@@ -1,9 +1,16 @@
 class DeclinedCreditCard < Base
   self.table_name = "ccdc".to_sym
 
-  #alias_attribute :publication,     :pub
-  #alias_attribute :sequence_number, :'cjseq#'
+  alias_attribute :pub_code,        :prspub
+  alias_attribute :batch_id,        :prbtch
+  alias_attribute :batch_date,      :prbdat
+  alias_attribute :account_number,  :prpact
+  alias_attribute :credit_amount,   :prcamt
+  alias_attribute :debit_amount,    :prdamt
+  alias_attribute :decline_status,  :prnsts
+  alias_attribute :decline_reson,   :prndcr
 
+  # attribute_names, 
   def method_name
   end
 end

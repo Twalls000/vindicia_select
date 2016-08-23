8b75ac3c2934ddd45b08666223c84fcf658b1672
Ryan Verhey <rverhey@gannett.com>
2016-08-23 15:53:39 -0400

    SLSMKT-969: Added test file for DeclinedCreditCard

---
+++ test/models/declined_credit_card_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DeclinedCreditCardTest < ActiveSupport::TestCase
+  def setup
+    # Setup
+  end
+
+  class Summary < DeclinedCreditCardTest
+    # Tests
+  end
+
+  class DeclinedTimestamp < DeclinedCreditCardTest
+    # Tests
+  end
+
+  class MerchantTransactionId < DeclinedCreditCardTest
+    # Tests
+  end
+end

2ce1cf583616881a63e36138994fd72bb2f5dead
Ryan Verhey <rverhey@gannett.com>
2016-08-23 15:18:16 -0400

    SLSMKT-969: Removed useless test

--- test/models/declined_credit_card_batch_test.rb
+++ test/models/declined_credit_card_batch_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class DeclinedCreditCardBatchTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end

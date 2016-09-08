1fe43e7dad702811b03186e5b080684b81012178
Ryan Verhey <rverhey@gannett.com>
2016-09-08 16:21:25 -0400

    SLSMKT-983: Cleaning up. Still need to figure out how to select Transactions based on gci_unit and pub_code

--- app/services/send_for_capture.rb
+++ app/services/send_for_capture.rb
@@ -3,17 +3,17 @@ class SendForCapture
   # This section of code is to send transactions to Vindicia Select
   #
   def self.process
-    sample_trans = DeclinedCreditCardTransaction.oldest_unsent.limit(1)
+    sample_trans = DeclinedCreditCardTransaction.oldest_unsent.limit(1).first
     mp = sample_trans.market_publication
-    pub_code = sample_trans.pub_code
-    gci_unit = sample_trans.gci_unit
+    pub_code = mp.pub_code
+    gci_unit = mp.gci_unit
 
     transactions_to_send =
       DeclinedCreditCardTransaction.oldest_unsent.
         by_gci_unit_pub_code(gci_unit, pub_code).
-        limit(sample_trans.market_publication.vindicia_batch_size)
+        limit(mp.market_publication.vindicia_batch_size)
 
-    SendForCaptureJob.perform_later transactions
+    SendForCaptureJob.perform_later transactions_to_send.to_a
   end
 
   def self.send_transactions_for_capture(transactions)

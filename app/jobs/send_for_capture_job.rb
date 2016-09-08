a8b6ba7c20d8e83b43706944af98218759007091
Ryan Verhey <rverhey@gannett.com>
2016-09-08 15:15:02 -0400

    SLSMKT-983: Updated job to send transactions for capture

--- app/jobs/send_for_capture_job.rb
+++ app/jobs/send_for_capture_job.rb
@@ -1,7 +1,7 @@
 class SendForCaptureJob < ActiveJob::Base
   queue_as :send_for_capture
 
-  def perform(*args)
-    # Do something later
+  def perform(transactions)
+    SendForCapture.send_transactions_for_capture(transactions)
   end
 end

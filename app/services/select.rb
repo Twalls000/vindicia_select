e06a99061a684f6445a01140e4f997f48ea2312a
Ryan Verhey <rverhey@gannett.com>
2016-09-14 10:20:54 -0400

    SLSMKT-1002: Using vindicia_fields method

--- app/services/select.rb
+++ app/services/select.rb
@@ -2,19 +2,7 @@ class Select
   def self.bill_transactions(transactions)
     transactions = Array(transactions)
 
-    param_transactions = transactions.map do |trans|
-      # TODO: remove the following lines when tokens supported by Vindicia
-      trans.payment_method_tokenized = false
-
-      trans.subscription_id = SecureRandom.hex
-      attrs = trans.attributes.except('created_at', 'updated_at', 'status')
-      attrs['payment_method_id'] = 'CreditCard'
-      attrs['status'] = attrs.delete('charge_status')
-      attrs['timestamp'] = DateTime.now
-      attrs.keys.each { |key| attrs[key.camelize(:lower)] = attrs.delete(key) }
-
-      attrs
-    end
+    param_transactions = transactions.map(&:vindicia_fields)
 
     # call(:bill_transactions, { transactions: param_transactions })
   end

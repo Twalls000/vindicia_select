1cce2746963258e9324a94e46c2b7f78a3ae7a46
Ryan Verhey <rverhey@gannett.com>
2016-09-14 14:51:34 -0400

    SLSMKT-1002: Single quotes

--- app/models/declined_credit_card_transaction.rb
+++ app/models/declined_credit_card_transaction.rb
@@ -32,7 +32,7 @@ class DeclinedCreditCardTransaction < ActiveRecord::Base
   }
 
   def vindicia_fields
-    attrs = attributes.except("batch_id", "charge_status", "created_at", "credit_card_number", "declined_credit_card_batch_id", "declined_timestamp", "gci_unit", "market_publication_id", "payment_method", "payment_method_tokenized", "pub_code", "status", "updated_at")
+    attrs = attributes.except('batch_id', 'charge_status', 'created_at', 'credit_card_number', 'declined_credit_card_batch_id', 'declined_timestamp', 'gci_unit', 'market_publication_id', 'payment_method', 'payment_method_tokenized', 'pub_code', 'status', 'updated_at')
 
     attrs.merge!({
       'status'                      => charge_status,

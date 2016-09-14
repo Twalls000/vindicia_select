864f119e49f84d44d8df44d6af100ad1f67cea3c
James Moore <jrmoore@gannett.com>
2016-09-14 10:18:11 -0400

    SLSMKT-970 renaming the division:

--- app/services/declined_batches.rb
+++ app/services/declined_batches.rb
@@ -71,7 +71,8 @@ class DeclinedBatches
       billing_address_line2:       declined_cc.billing_address_line2.squeeze,
       billing_addr_city:           declined_cc.billing_addr_city.squeeze,
       billing_address_district:    declined_cc.billing_address_district,
-      billing_address_postal_code: declined_cc.billing_address_postal_code
+      billing_address_postal_code: declined_cc.billing_address_postal_code,
+      division_id:                 declined_cc.division_id
     }
   end
 end

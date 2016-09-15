e6bc3bb09b0cc4093c152a10b87a1cae4b743cb7
James Moore <jrmoore@gannett.com>
2016-09-15 13:16:25 -0400

    SLSMKT-985 tests

--- test/models/declined_credit_card_transaction_test.rb
+++ test/models/declined_credit_card_transaction_test.rb
@@ -23,10 +23,10 @@ class DeclinedCreditCardTransactionTest < ActiveSupport::TestCase
       assert_equal [:entry, :pending, :in_error], DeclinedCreditCardTransaction.aasm.states.map(&:name)
     end
     test "workflow events" do
-      assert_equal [:sent_to_vindicia, :mark_in_error], DeclinedCreditCardTransaction.aasm.events.map(&:name)
+      assert_equal [:sent_to_vindicia, :mark_in_error, :captured_funds, :failed_to_capture_funds], DeclinedCreditCardTransaction.aasm.events.map(&:name)
     end
     test "workflow permitted based on entry state" do
-      assert_equal [:sent_to_vindicia, :mark_in_error], @trans.aasm.events(:permitted => true).map(&:name)
+      assert_equal [:sent_to_vindicia, :mark_in_error, :captured_funds, :failed_to_capture_funds], @trans.aasm.events(:permitted => true).map(&:name)
     end
     test "workflow permitted based on pending, in_error state" do
       ["pending", "in_error"].each do |what|

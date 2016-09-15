a4657e08a7faeca7ce1ed613a871c44e8b7c0e62
James Moore <jrmoore@gannett.com>
2016-09-15 13:53:07 -0400

    SLSMKT-985 added job to whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 # Use this file to easily define all of your cron jobs.
 #
+env :PATH, ENV['PATH']
+env :GEM_PATH, ENV['GEM_PATH']
 every 5.minutes do
-  
+  rake "process_declined_credit_cards"
 end

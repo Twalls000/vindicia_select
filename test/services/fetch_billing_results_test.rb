cd1a05fe8d9f46fa7509c8a545f23227c9a6ccac
Ryan Verhey <rverhey@gannett.com>
2016-09-27 10:17:30 -0400

    SLSMKT-985b: Added some tests for FetchBillingResults

---
+++ test/services/fetch_billing_results_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class FetchBillingResultsTest < ActiveSupport::TestCase
+  def setup
+
+  end
+
+  class Initialize < FetchBillingResultsTest
+    test 'an error is thrown if no start_timestamp is passed' do
+      exception = assert_raises(KeyError) { FetchBillingResults.new }
+      assert_equal "key not found: :start_timestamp", exception.message
+    end
+
+    test 'an error is thrown if no page_size is passed' do
+      exception = assert_raises(KeyError) { FetchBillingResults.new(start_timestamp: DateTime.now) }
+      assert_equal "key not found: :page_size", exception.message
+    end
+
+    test 'the default value for end_timestamp is nil' do
+      fbr = FetchBillingResults.new(start_timestamp: DateTime.now, page_size: 10)
+      assert_nil fbr.end_timestamp
+    end
+
+    test 'the default value for page is 0' do
+      fbr = FetchBillingResults.new(start_timestamp: DateTime.now, page_size: 10)
+      assert_equal 0, fbr.page
+    end
+  end
+
+  class Process < FetchBillingResultsTest
+    # Tests
+  end
+
+  class FetchBillingResultsMethod < FetchBillingResultsTest
+    # Tests
+  end
+
+  class ProcessResponse < FetchBillingResultsTest
+    # Tests
+  end
+end

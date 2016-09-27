86689c5dc832d96ba936806c26f17e0b43f56798
James Moore <jrmoore@gannett.com>
2016-09-27 09:52:27 -0400

    slsmkt-983

--- test/models/declined_credit_card_transaction_test.rb
+++ test/models/declined_credit_card_transaction_test.rb
@@ -20,12 +20,12 @@ class DeclinedCreditCardTransactionTest < ActiveSupport::TestCase
       assert @trans.entry?
     end
     test "workflow states" do
-      assert_equal [:entry, :queued_to_send, :pending, :in_error],
+      assert_equal [:entry, :queued_to_send, :pending, :in_error, :processed, :printed_bill, :no_reply],
         DeclinedCreditCardTransaction.aasm.states.map(&:name)
     end
     test "workflow events" do
       assert_equal [:queue_to_vindicia, :send_to_vindicia, :error_sending_to_vindicia,
-        :mark_in_error, :captured_funds, :failed_to_capture_funds],
+        :mark_in_error, :captured_funds, :failed_to_capture_funds, :failed_to_get_reply],
         DeclinedCreditCardTransaction.aasm.events.map(&:name)
     end
     test "workflow permitted based on entry state" do
@@ -34,7 +34,7 @@ class DeclinedCreditCardTransactionTest < ActiveSupport::TestCase
     end
     test "workflow permitted based on pending" do
       @trans.status="pending"
-      assert_equal [:captured_funds, :failed_to_capture_funds],
+      assert_equal [:captured_funds, :failed_to_capture_funds, :failed_to_get_reply],
         @trans.aasm.events(:permitted => true).map(&:name)
     end
     test "workflow permitted based on in error" do

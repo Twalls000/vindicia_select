e2e1e5daf74e026a4ac53504f73418d6e70a2a88
James Moore <jrmoore@gannett.com>
2016-09-28 09:34:55 -0400

    SLSMKT-985 whenever log

--- lib/tasks/process_failed_billing_results.rb
+++
@@ -1,9 +0,0 @@
-# Run rake process_declined_credit_cards
-# Run rake process_declined_credit_cards RAILS_ENV=production
-# Example "run bundle exec rake process_declined_credit_cards"
-
-task(:process_failed_billing_results => :environment) do
-
-  FailedBillingResults.process
-
-end

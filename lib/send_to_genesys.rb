17ff326f2a543e729ab5722bd7e673b1c1b9c4b3
Ryan Verhey <rverhey@gannett.com>
2016-10-05 16:01:52 -0400

    SLSMKT-1007: Updating Genesys record with new charge status

--- lib/send_to_genesys.rb
+++ lib/send_to_genesys.rb
@@ -24,10 +24,24 @@ module SendToGenesys
 
     def set_as_successful(transaction)
       # send transaction details to Genesys
+      update_genesys_record(transaction, "Captured") # Not sure if right keyword
     end
 
     def set_to_printed_bill(transaction)
       # notify Genesys that the account should be set to printed bill
+      update_genesys_record(transaction, "Printed Bill") # Not sure if right keyword
+    end
+
+    private
+
+    def update_genesys_record(transaction, vsaust)
+      card = get_genesys_record_by_merchant_transaction_id(transaction.merchant_transaction_id)
+      card.vsaust = vsaust
+      card.save
+    end
+
+    def get_genesys_record_by_merchant_transaction_id(merchant_transaction_id)
+      self.where(vstrid: merchant_transaction_id)
     end
 
   end

253d80263553c474a49e8d9abfd2bbeb6de94900
Ryan Verhey <rverhey@gannett.com>
2016-10-12 16:22:20 -0400

    Refactoring

--- app/services/select.rb
+++ app/services/select.rb
@@ -1,21 +1,16 @@
 class Select
   def self.bill_transactions(transactions)
-    transactions = Array(transactions)
-
-    param_transactions = transactions.map(&:vindicia_fields)
-    call(:bill_transactions, { transactions: param_transactions })
-  end
+   call(:bill_transactions, { transactions: Array(transactions).map(&:vindicia_fields) })
+ end
 
   def self.fetch_billing_results(timestamp, end_timestamp = nil, page = 0, page_size = 50)
-    timestamp = date_to_vindicia timestamp
     end_timestamp = date_to_vindicia(end_timestamp) if end_timestamp
-
-    call(:fetch_billing_results, { timestamp: timestamp, end_timestamp: end_timestamp, page: page, page_size: page_size })
+    call(:fetch_billing_results, { timestamp: date_to_vindicia(timestamp), end_timestamp: end_timestamp, page: page, page_size: page_size })
   end
 
   def self.call(method_name, params = {})
-    resp = Vindicia::Connection.call("Select", method_name.to_sym, params)
-    response_handler resp, (method_name == :bill_transactions ? true : [])
+    response_handler Vindicia::Connection.call("Select", method_name.to_sym, params),
+    (method_name == :bill_transactions ? true : [])
   end
 
   def self.date_to_vindicia(date)
@@ -29,14 +24,14 @@ class Select
   def self.response_handler(response, default_return = true)
     case response
     when Hash && { return_code: response.try(:[], :return_code), soap_id: response.try(:[], :soap_id) }
-      return response
+      response
     when Savon::Response
       return_val = response.hash[:envelope][:body].first[1][:return]
       raise("Error with soap_id #{return_val[:soap_id]} (code #{return_val[:return_code]}): #{return_val[:return_string]}")
     when Vindicia::Transaction
-      return Array(response)
+      Array(response)
     else
-      return response
+      response
     end
   end
 end

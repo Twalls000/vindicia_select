c53223f7af672900f21c3ebd80b33bb9ee59229b
Ryan Verhey <rverhey@gannett.com>
2016-10-19 09:08:50 -0400

    SLSMKT-1023: Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014144658) do
+ActiveRecord::Schema.define(version: 20161019130346) do
 
   create_table "audit_trails", force: :cascade do |t|
     t.string   "event"
@@ -117,6 +117,8 @@ ActiveRecord::Schema.define(version: 20161014144658) do
     t.integer  "days_before_failure"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "lock_version"
+    t.integer  "fetch_page_number"
   end
 
 end

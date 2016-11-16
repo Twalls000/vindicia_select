179fe75509469b6a186c20dbf35042fde9531ba7
Ryan Verhey <rverhey@gannett.com>
2016-11-16 16:17:56 -0500

    SLSMKT-1051: Added method for sending event to Datadog

--- app/services/data_dog.rb
+++ app/services/data_dog.rb
@@ -1,8 +1,19 @@
 module DataDog
   DATADOG_API_KEY = ENV["DATADOG_API_KEY"]
-  DATADOG_APP_KEY = ENV["VS_DATADOG_APP_KEY"]
 
   def self.new_client
-    Dogapi::Client.new(DATADOG_API_KEY,DATADOG_APP_KEY)
+    Dogapi::Client.new(DATADOG_API_KEY)
+  end
+
+  def self.send_event(message_text,message_title,alert_type,tags)
+    tags = ["vindicia_select", "environment:#{Rails.env}"] + tags
+    client = new_client
+    event = Dogapi::Event.new(message_text,
+      msg_title: "Vindicia Select #{message_title}",
+      tags: tags,
+      alert_type: alert_type
+    )
+
+    client.emit_event event
   end
 end

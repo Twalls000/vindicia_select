cf366f06c3fdec952fe9aeb8d9fbbb3a1ce818c1
Ryan Verhey <rverhey@gannett.com>
2016-11-17 12:09:31 -0500

    SLSMKT-1051: Moved event creation to a new method

--- app/services/data_dog.rb
+++ app/services/data_dog.rb
@@ -5,14 +5,18 @@ module DataDog
     Dogapi::Client.new(DATADOG_API_KEY)
   end
 
+  def self.new_event(message_text,message_title,alert_type,tags)
+    Dogapi::Event.new(message_text,
+      msg_title: message_title,
+      alert_type: alert_type,
+      tags: tags
+    )
+  end
+
   def self.send_event(message_text,message_title,alert_type,tags)
     tags = ["vindicia_select", "environment:#{Rails.env}"] + tags
     client = new_client
-    event = Dogapi::Event.new(message_text,
-      msg_title: "Vindicia Select #{message_title}",
-      tags: tags,
-      alert_type: alert_type
-    )
+    event = new_event(message_text, "Vindicia Select #{message_title}", alert_type, tags)
 
     client.emit_event event
   end

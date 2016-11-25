4c67c36ac6aa6ca04b9434bec9b1851a81960ad9
Ryan Verhey <rverhey@gannett.com>
2016-11-25 10:32:06 -0500

    SLSMKT-1055: Added tests for HandleInError::handle

--- test/services/handle_in_error_test.rb
+++ test/services/handle_in_error_test.rb
@@ -8,7 +8,80 @@ class HandleInErrorTest < ActiveJob::TestCase
   end
 
   class Handle < HandleInErrorTest
-    # Tests
+    class WhenMoreThanOneAuditTrail < Handle
+      test 'the transaction is sent to Genesys as failed' do
+        @trans.audit_trails.create(event: "Some event")
+        @trans.audit_trails.create(event: "Some other event")
+        verify = ->(trans){
+          assert_equal @trans.id, trans.id
+        }
+
+        verify_class_method HandleInError, :send_failed_to_genesys, verify do
+          HandleInError.handle([@trans.id])
+        end
+        trans = DeclinedCreditCardTransaction.find @trans.id
+        assert_equal "Failed", trans.charge_status
+      end
+    end
+
+    class WhenErrorMatchesKnownErrors < Handle
+      def teardown
+        trans = DeclinedCreditCardTransaction.find @trans.id
+        assert_equal "entry", trans.status
+      end
+
+      test 'SSL errors are set to entry status' do
+        @trans.audit_trails.create(event: HandleInError::SSL_ERROR_MESSAGE)
+
+        HandleInError.handle([@trans.id])
+      end
+
+      test 'Vindicia 400 errors are set to entry status' do
+        vindicia_400_error = "Vindicia code 400: [specific message] that has already been processed by CashBox Select"
+        @trans.audit_trails.create(event: vindicia_400_error)
+
+        HandleInError.handle([@trans.id])
+      end
+    end
+
+    class WhenErrorIsUnknown < Handle
+      def setup
+        super
+        @trans.audit_trails.create(event: "this is some unknown error")
+      end
+
+      test 'the transaction is sent to Genesys as failed' do
+        verify = ->(trans){
+          assert_equal @trans.id, trans.id
+        }
+
+        DataDog.stub :send_event, true do
+          verify_class_method HandleInError, :send_failed_to_genesys, verify do
+            HandleInError.handle([@trans.id])
+          end
+        end
+
+        trans = DeclinedCreditCardTransaction.find @trans.id
+        assert_equal "Failed", trans.charge_status
+      end
+
+      test 'a DataDog event is created' do
+        verify = ->(msg, title, alert_type, tags){
+          ex_msg = "Transaction with ID #{@trans.id}:\n\n#{@trans.audit_trails.map(&:event).join("\n")}"
+          ex_title = "Encountered unknown error when handling in_error transaction"
+          assert_equal ex_msg, msg
+          assert_equal ex_title, title
+          assert_equal "error", alert_type
+          assert_equal ["handle_in_error"], tags
+        }
+
+        verify_class_method DataDog, :send_event, verify do
+          HandleInError.stub :send_failed_to_genesys, true do
+            HandleInError.handle([@trans.id])
+          end
+        end
+      end
+    end
   end
 
   class SendFailedToGenesys < HandleInErrorTest

04c906a5326719bd153dd1ab29660701088c87b3
Ryan Verhey <rverhey@gannett.com>
2016-12-13 11:50:24 -0500

    SLSMKT-1067: Setting up variables for view

--- app/mailers/status_update_mailer.rb
+++ app/mailers/status_update_mailer.rb
@@ -3,6 +3,23 @@ class StatusUpdateMailer < ApplicationMailer
           from: "vindicia_select_#{Rails.env}@gannett.com"
 
   def status_email
+    set_up_variables
+
     mail subject: "Vindicia Select #{Rails.env.capitalize} Status"
+  end
+
+  private
+
+  def set_up_variables
+    @audit_trail_errors = AuditTrail.joins(:declined_credit_card_transaction).where("declined_credit_card_transactions.status = 'in_error'").group(:declined_credit_card_transaction_id).map(&:event)
+
+    @in_error_jobs = Delayed::Job.where("last_error is not NULL").map(&:last_error)
+
+    @non_added_sites = Site.where(gci_unit: Site.select(:gci_unit).map(&:gci_unit) - MarketPublication.select(:gci_unit).map(&:gci_unit)).order(:name)
+    @non_added_sites = @non_added_sites.inject({}) do |hash,site|
+      hash[site.name] =
+        DeclinedCreditCard.on_db(site.gci_unit).count rescue "No CCVC"
+      hash
+    end
   end
 end

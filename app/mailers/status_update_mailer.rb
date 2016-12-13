d5127205779eb5188c0ba4578e2446cd967ff828
Ryan Verhey <rverhey@gannett.com>
2016-12-13 09:08:48 -0500

    SLSMKT-1067: Generated StatusUpdateMailer

---
+++ app/mailers/status_update_mailer.rb
@@ -0,0 +1,2 @@
+class StatusUpdateMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/status_update_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/status_update_mailer
+class StatusUpdateMailerPreview < ActionMailer::Preview
+
+end

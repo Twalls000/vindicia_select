eb5b65981dddbcf5675ea1e57f8c3d8cb1d59153
Ryan Verhey <rverhey@gannett.com>
2016-12-13 11:49:08 -0500

    SLSMKT-1067: To be able to preview emails without sending them

--- test/mailers/previews/status_update_mailer_preview.rb
+++ test/mailers/previews/status_update_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/status_update_mailer
 class StatusUpdateMailerPreview < ActionMailer::Preview
-
+  def status
+    StatusUpdateMailer.status_email
+  end
 end

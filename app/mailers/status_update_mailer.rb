3b6f4c09559d94568ba30d7c75da53480b0fc36d
Ryan Verhey <rverhey@gannett.com>
2016-12-13 10:07:38 -0500

    SLSMKT-1067: Added status_email method

--- app/mailers/status_update_mailer.rb
+++ app/mailers/status_update_mailer.rb
@@ -1,2 +1,8 @@
 class StatusUpdateMailer < ApplicationMailer
+  default to:   Rails.env.production? ? "wspencer@gannett.com,rverhey@gannett.com" : "rverhey@gannett.com",
+          from: "vindicia_select_#{Rails.env}@gannett.com"
+
+  def status_email
+    mail subject: "Vindicia Select #{Rails.env.capitalize} Status"
+  end
 end

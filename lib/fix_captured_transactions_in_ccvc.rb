6d4fb494a59bfc7ac4b19b060dfe03ffbb778adf
Ryan Verhey <rverhey@gannett.com>
2017-01-20 12:07:13 -0500

    SLSMKT-1077: Added FixCapturedTransactionsInCcvc

---
+++ lib/fix_captured_transactions_in_ccvc.rb
@@ -0,0 +1,9 @@
+# January 2017
+# This class has methods that attempt to correct an issue where transactions
+# were sent back to CCVC when they shouldn't have. Some transcactions came back
+# from Vindicia as captured, but that wasn't updated in CCVC. These methods will
+# update the CCVC records that were affected.
+
+class FixCapturedTransactionsInCcvc
+
+end

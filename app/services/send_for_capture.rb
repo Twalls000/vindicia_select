347272e83490b249ace1b6614cc864c806313c44
Ryan Verhey <rverhey@gannett.com>
2017-01-24 16:03:30 -0500

    SLSMKT-1075: Refactored reused code into lambdas

--- app/services/send_for_capture.rb
+++ app/services/send_for_capture.rb
@@ -36,16 +36,17 @@ class SendForCapture
           vtvr = response.select { |r| r.is_a?(Vindicia::TransactionValidationResponse) &&
               r.merchant_transaction_id == t.merchant_transaction_id }.first
           if vtvr
-            t.audit_trails.build(event: "Vindicia code #{vtvr.code}: #{vtvr.description}")
-            t.soap_id = vtvr.soap_id
-            t.error_sending_to_vindicia
+            functions = ->{
+              t.audit_trails.build(event: "Vindicia code #{vtvr.code}: #{vtvr.description}")
+              t.soap_id = vtvr.soap_id
+              t.error_sending_to_vindicia if t.may_error_sending_to_vindicia?
+            }
+            functions.call
             begin
               t.save
             rescue ActiveRecord::StaleObjectError => e
               t.reload
-              t.audit_trails.build(event: "Vindicia code #{vtvr.code}: #{vtvr.description}")
-              t.soap_id = vtvr.soap_id
-              t.error_sending_to_vindicia if t.may_error_sending_to_vindicia?
+              functions.call
               t.save if t.sending?
             end
           else
@@ -59,27 +60,31 @@ class SendForCapture
         end
       elsif response.is_a?(Hash) && response[:soap_id]
         transactions.each do |t|
-          t.soap_id = response[:soap_id]
-          t.send_to_vindicia
+          functions = ->{
+            t.soap_id = response[:soap_id]
+            t.send_to_vindicia if t.may_send_to_vindicia?
+          }
+          functions.call
           begin
             t.save
           rescue ActiveRecord::StaleObjectError => e
             t.reload
-            t.soap_id = response[:soap_id]
-            t.send_to_vindicia if t.may_send_to_vindicia?
+            functions.call
             t.save if t.sending?
           end
         end
       else
         transactions.each do |t|
-          t.audit_trails.build(event: "Failed to send", exception: response)
-          t.error_sending_to_vindicia
+          functions = ->{
+            t.audit_trails.build(event: "Failed to send", exception: response)
+            t.error_sending_to_vindicia if t.may_error_sending_to_vindicia?
+          }
+          functions.call
           begin
             t.save
           rescue ActiveRecord::StaleObjectError => e
             t.reload
-            t.audit_trails.build(event: "Failed to send", exception: response)
-            t.error_sending_to_vindicia if t.may_error_sending_to_vindicia?
+            functions.call
             t.save if t.sending?
           end
         end

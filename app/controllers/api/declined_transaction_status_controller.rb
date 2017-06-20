b66fa004c7f63c1cd9663b0521cf5e7da5469fb0
Ryan Verhey <rverhey@gannett.com>
2017-06-20 08:21:07 -0400

    SLSMKT-1089: Adding new return values

--- app/controllers/api/declined_transaction_status_controller.rb
+++ app/controllers/api/declined_transaction_status_controller.rb
@@ -4,7 +4,10 @@ class Api::DeclinedTransactionStatusController < ApiController
     @transaction = DeclinedCreditCardTransaction.find_by(id: params[:id])
 
     if @transaction && @transaction.gci_unit.try(:upcase) == "PHX"
-      render json: {status: @transaction.summary_status}
+      json = { status:                  @transaction.summary_status,
+               select_transaction_id:   @transaction.select_transaction_id,
+               merchant_transaction_id: @transaction.merchant_transaction_id }
+      render json: json
     else
       render json: {}, status: 404
     end

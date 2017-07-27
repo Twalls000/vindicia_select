525d1e712c4a4a92e25b0756286e09ee00656d9e
Ryan Verhey <rverhey@gannett.com>
2017-07-27 11:37:27 -0400

    GCMAIN-2043: Fixing an issue where MTID resolved as a non-zero number

--- app/controllers/api/declined_transaction_status_controller.rb
+++ app/controllers/api/declined_transaction_status_controller.rb
@@ -1,8 +1,21 @@
 class Api::DeclinedTransactionStatusController < ApiController
 
   def show
-    query = ["id = :query OR merchant_transaction_id = :query", { query: params[:id] }]
-    @transaction = DeclinedCreditCardTransaction.where(*query).first
+    # If you turn a string into an integer then back into a string again, if it
+    # stays the same as the original then it was most likely a number
+    # originally, so it will be the ID, since our merchant_transaction_ids are
+    # alpha-numeric. But, if it was alpha-numeric to begin with, turning it into
+    # an integer then back in the string will change it, so it will not match,
+    # and that will be the merchant_transaction_id.
+    # ex: "123-ABC-456" -> 123 -> "123" != "123-ABC-456"
+    #     "15"          -> 15  -> "15"  == "15"
+    query =
+      if params[:id].to_i.to_s == params[:id]
+        { id: params[:id] }
+      else
+        { merchant_transaction_id: params[:id] }
+      end
+    @transaction = DeclinedCreditCardTransaction.where(query).first
 
     if @transaction && @transaction.gci_unit.try(:upcase) == MarketPublication::PHOENIX
       json = { id:                      @transaction.id,

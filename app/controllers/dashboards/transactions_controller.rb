9ccf50b214d379392f69dc6c12a4faf397239b1b
Ryan Verhey <rverhey@gannett.com>
2017-08-04 10:12:32 -0400

    GCMAIN-2053: Added show method

--- app/controllers/dashboards/transactions_controller.rb
+++ app/controllers/dashboards/transactions_controller.rb
@@ -1,6 +1,10 @@
 class Dashboards::TransactionsController < ApplicationController
   def index
   end
+
   def show
+    @transaction = DeclinedCreditCardTransaction.find_by(id: params[:id])
+    @audit_trails = @transaction.audit_trails
+  end
   end
 end

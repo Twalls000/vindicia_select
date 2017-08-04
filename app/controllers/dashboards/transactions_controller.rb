4c00f21eeb57d556e7024798f7f28b5455e1fbbc
Ryan Verhey <rverhey@gannett.com>
2017-08-04 10:12:46 -0400

    GCMAIN-2053: Added search method

--- app/controllers/dashboards/transactions_controller.rb
+++ app/controllers/dashboards/transactions_controller.rb
@@ -6,5 +6,22 @@ class Dashboards::TransactionsController < ApplicationController
     @transaction = DeclinedCreditCardTransaction.find_by(id: params[:id])
     @audit_trails = @transaction.audit_trails
   end
+
+  def search
+    @query =
+      if params[:trans_id].to_i.to_s == params[:trans_id]
+        { id: params[:trans_id] }
+      else
+        { merchant_transaction_id: params[:trans_id] }
+      end
+
+    @transaction = DeclinedCreditCardTransaction.select(:id).where(@query).first
+
+    if @transaction
+      redirect_to dashboards_transaction_url(@transaction.id)
+    else
+      flash[:error] = "Transaction not found"
+      render :index
+    end
   end
 end

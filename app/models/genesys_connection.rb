7c322afe4f08978f7bf7b90f97e414b39318153a
Ryan Verhey <rverhey@gannett.com>
2017-10-02 11:12:25 -0400

    GCMAIN-2182: Adding methods

--- app/models/genesys_connection.rb
+++ app/models/genesys_connection.rb
@@ -1,2 +1,22 @@
 class GenesysConnection < ActiveRecord::Base
+  DEFAULTS = ActiveRecord::Base.configurations["#{ Rails.env }_defaults"]
+
+  validates :gci_unit, uniqueness: true
+
+  # Adding methods for each of the default configurations. Uses the default
+  # unless there is a value in the table.
+  DEFAULTS.each do |attr_name,value|
+    define_method(attr_name.to_sym) do
+      super() || value
+    end
+  end
+
+  def self.find_by_unit(unit)
+    find_by gci_unit: unit.to_i
+  end
+
+  def configuration
+    attrs = attributes.except("id","gci_unit","created_at","updated_at").reject { |k,v| v.blank? }
+    DEFAULTS.merge attrs
+  end
 end

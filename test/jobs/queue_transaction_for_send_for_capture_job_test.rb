d45d8fcaf1703d50a047003aff18fb84b339eced
Ryan Verhey <ryan90v@gmail.com>
2019-01-16 10:07:52 -0500

    GCMAIN-2607: Added tests for new job

--- test/jobs/queue_transaction_for_send_for_capture_job_test.rb
+++ test/jobs/queue_transaction_for_send_for_capture_job_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QueueTransactionForSendForCaptureJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @trans = declined_credit_card_transactions(:handle_in_error).dup
+    @trans.save
+  end
+
+  test "sets transaction status to entry to be picked up by automated process" do
+    assert @trans.in_error?
+    QueueTransactionForSendForCaptureJob.perform_now(@trans)
+    assert @trans.reload.entry?
+  end
 end
